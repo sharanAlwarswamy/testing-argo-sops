e53639bfbb1008ce3dc279077e96baadbfbbd07b
sharanAlwarswamy <sharan.alwar@outlook.com>
2025-01-25 16:54:38 +0530

    hehe

--- app/app.py
+++ app/app.py
@@ -1,11 +1,15 @@
 from flask import Flask, render_template
 import os
+from dotenv import load_dotenv
 
 app = Flask(__name__)
+
+load_dotenv()
 
 @app.route("/")
 def index():
     name = os.getenv("NAME", "DefaultName")
+    print(name)
     return render_template("index.html", name=name)
 
 if __name__ == "__main__":
